a37a97e4427b7d1e6e423dd3e5ebb4d46a92bbea
JCornelison <gitKraken@VashonDesign.com>
2022-01-03 10:16:28 -0800

    next day

--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,12 +1,15 @@
 import { DOCUMENT, JsonPipe } from '@angular/common'
 import { AfterViewInit, Component, Inject, OnInit, isDevMode } from '@angular/core'
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import {MDCSlider} from '@material/slider'
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { Observable, pipe } from 'rxjs'
 import { debounceTime, distinctUntilChanged, map, startWith, switchMap } from 'rxjs/operators'
 
 import { SettingsComponent } from '../settings/settings.component'
 import { FieldReportService, FieldReportStatuses, RangerService, RangerType, TeamService, TeamType } from '../shared/services/'
+
+//@use "@material/slider/styles"
 
 @Component({
   selector: 'rangertrak-entry',
@@ -16,18 +19,18 @@ import { FieldReportService, FieldReportStatuses, RangerService, RangerType, Tea
 })
 export class EntryComponent implements OnInit, AfterViewInit {
 
-  // BUG: Following is a dupl of FieldReportStatuses
-  //public fieldReportStatus = ['None', 'Normal', 'Need Rest', 'Urgent', 'Objective Update', 'Check-in', 'Check-out']  // TODO: Allow changing list & default of statuses in settings?!
   callsignCtrl = new FormControl()
   filteredRangers: Observable<RangerType[]> //| null
   rangers: RangerType[] = []
-  teams: TeamType[]   // TODO: Now what to do with the list of Teams?!!!
   fieldReportService
   fieldReportStatuses
   setting = SettingsComponent.AppSettings
   entryDetailsForm!: FormGroup
   smartInput = new FormControl({value:'', disabled:true})
   smartDisabled = true
+  numFakes = 30
+  //const fakeSlider = new MDCSlider(document.querySelector('.mdc-slider'));
+
 
   constructor(
     private formBuilder: FormBuilder,
@@ -37,23 +40,22 @@ export class EntryComponent implements OnInit, AfterViewInit {
     teamService: TeamService,
     @Inject(DOCUMENT) private document: Document) {   //, private service: PostService) {
 
-    // REVIEW: Or should this be done in ngOnInit()?
     this.rangers = rangerService.getRangers() // TO DO: or getActiveRangers?!
     this.fieldReportService = fieldReportService
-    this.teams = teamService.getTeams()
     this.fieldReportStatuses = FieldReportStatuses
+
+    this.smartInput.valueChanges.pipe(debounceTime(500)).subscribe(smartest => this.getABCFromServer(smartest))
+
     // https://material.angular.io/components/autocomplete/examples#autocomplete-overview
     this.filteredRangers = this.callsignCtrl.valueChanges.pipe(
       startWith(''),
       map(callsign => (callsign ? this._filterRangers(callsign) : this.rangers.slice())),
     )
-
-    this.smartInput.valueChanges.pipe(debounceTime(500)).subscribe(smartest => this.getABCFromServer(smartest))
   }
 
   private _filterRangers(value: string): RangerType[] {
     const filterValue = value.toLowerCase()
-    //this.entryDetailsForm.controls['callsignCtrl'].setValue(filterValue)   // TODO: MAT input field not automatically set into entryForm above
+    this.entryDetailsForm.value.callsign = filterValue // TODO: Have MAT input field auto sync w/ callsign
     return this.rangers.filter((ranger1) => ranger1.callsign.toLowerCase().includes(filterValue))
   }
 
@@ -67,17 +69,11 @@ export class EntryComponent implements OnInit, AfterViewInit {
 
     this.entryDetailsForm = this.formBuilder.group({
       id: -1,
-      callsign: ['EvilCallSign'],  // TODO: Not tied to the material design input field...
-      team: ['T2'],
-      address: ['default location (original)'],
-      lat: [this.setting.DEF_LAT,
-      Validators.required,
-        //Validators.minLength(4)
-      ],
-      long: [this.setting.DEF_LONG,
-      Validators.required,
-        //Validators.minLength(4)
-      ],
+      callsign: ['ngOnInitCallSign'],  // TODO: Not tied to the material design input field...
+      team: ['T1'],
+      address: ['default location (ngOnInit)'],
+      lat:  [this.setting.DEF_LAT,  Validators.required], //Validators.minLength(4)
+      long: [this.setting.DEF_LONG, Validators.required], //Validators.minLength(4)
       date: [new Date()],
       status: [FieldReportStatuses[0]],   // TODO: Allow changing list & default of statuses in settings?!
       note: ['']
@@ -119,12 +115,10 @@ export class EntryComponent implements OnInit, AfterViewInit {
   resetForm() {
     console.log("Resetting form...")
 
-
     this.entryDetailsForm = this.formBuilder.group({
       id: -2,
-      callsign: ['resetCallSign'],  // TODO: Not tied to the material design input field...
+      callsign: ['resetFormCallSign'],  // TODO: Not tied to the material design input field...
       team: ['T0'],
-      //whereFormModel: this.formBuilder.group({
       address: ['default location (reset)'],
       lat: [this.setting.DEF_LAT,
       Validators.required,
@@ -134,42 +128,26 @@ export class EntryComponent implements OnInit, AfterViewInit {
       Validators.required,
         //Validators.minLength(4)
       ],
-      //      }),
-      //    whenFormModel: this.formBuilder.group({
       date: [new Date()],
-      //  }),
-      //whatFormModel: this.formBuilder.group({
       status: [FieldReportStatuses[0]],   // TODO: Allow changing list & default of statuses in settings?!
       notes: ['']
-      //})
     })
-
-
-    // BUG: none of the following work...
-    // calls = this.document.getElementById("callsign")
-    //if (calls) calls.innerText = ''
-    //this.entryDetailsForm.controls['#callsign'].reset
-    //this.entryDetailsForm.controls['#callsign'].reset
-    //this.entryDetailsForm.controls['#callsign'].setValue('')
-    //this.entryDetailsForm.controls['derivedAddress'].setValue('New ddddddddDerived Address')
   }
 
-  generateFakeFieldReports(num = 30) {
+  generateFakeFieldReports(num = this.numFakes) {
     this.fieldReportService.generateFakeData(num)
     console.log(`Generated ${num} FAKE Field Reports`)
   }
 
   updateLocation() {
+    console.log("updateLocation() running")
     //this.entryDetailsForm.get(['', 'name'])
     //this.entryDetailsForm.controls['derivedAddress'].setValue('New Derived Address')
-
     var addr = this.document.getElementById("derivedAddress")
     if (addr) { addr.innerHTML = "New What3Words goes here!" }
   }
 
   ngAfterViewInit() {
     console.log("ngAfterViewInit")
-
-    //console.debug(this.divs)
   }
 }
