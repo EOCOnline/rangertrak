206630b66651a6bd0ed235d2b0714d7b6bf12653
JCornelison <gitKraken@VashonDesign.com>
2022-01-17 20:52:09 -0800

    consolidate excel import ideas to Rangers

--- src/app/rangers/rangers.component.ts
+++ src/app/rangers/rangers.component.ts
@@ -5,6 +5,20 @@ import { csvImport } from './csvImport'
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AlertsComponent } from '../alerts/alerts.component';
 
+
+/* Following gets:
+index.js:553 [webpack-dev-server] WARNING
+D:\Projects\RangerTrak\rangertrak\src\app\log\log.component.ts depends on 'xlsx'. CommonJS or AMD dependencies can cause optimization bailouts.
+For more info see: https://angular.io/guide/build#configuring-commonjs-dependencies */
+import * as XLSX from 'xlsx';
+
+/* xlsx.js (C) 2013-present SheetJS -- http://sheetjs.com */
+// https://github.com/SheetJS/SheetJS.github.io
+// D:\Projects\ImportExcel\sheetjs-master\demos\angular2\src\app\sheetjs.component.ts
+// D:\Projects\ImportExcel\sheetjs-master\demos\angular2\ionic.ts
+
+type AOA = any[][]  // array of arrays
+
 @Component({
   selector: 'rangertrak-rangers',
   templateUrl: './rangers.component.html',
@@ -24,6 +38,7 @@ export class RangersComponent implements OnInit {
   maxSeperatorWarnings = 3
   now: Date
   settings
+  data: any[][] = [[1,2,3],[4,5,6]];
 
   // https://www.ag-grid.com/angular-data-grid/grid-interface/#grid-options-1
   gridOptions = {
@@ -92,6 +107,15 @@ export class RangersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+
+
+    if (!SettingsService.debugMode) {
+      this.displayHide("Log__Excel")
+    }
+
+
+
     this.rangers = this.rangerService.GetRangers()  // NOTE: zeros out the array!!!!
     //this.rangerService.generateFakeData(10) // NOTE: number is ignored currently
     console.log(`ngInit: ${this.rangers.length} Rangers retrieved from Local Storage and/or fakes generated`)
@@ -109,6 +133,25 @@ export class RangersComponent implements OnInit {
 
     //console.log("Rangers Form initialized at ", Date())
   }
+
+/*
+    // TODO: Move these into a utility class?
+    displayHide(htmlElementID: string) {
+      let e = this.document.getElementById(htmlElementID)
+      if (e) {
+        e.style.visibility = "hidden";
+      }
+    }
+
+    displayShow(htmlElementID: string) {
+      let e = this.document.getElementById(htmlElementID)
+      if (e) {
+        e.style.visibility = "visible";
+      }
+    }
+  */
+
+
 
   onGridReady = (params: any) => {
     this.gridApi = params.api;
@@ -254,6 +297,102 @@ export class RangersComponent implements OnInit {
       e.style.visibility = "visible";
     }
   }
+
+
+
+
+
+
+
+
+
+  read(ab: ArrayBuffer) {
+    /* read workbook */
+    const wb: XLSX.WorkBook = XLSX.read(new Uint8Array(ab), { type: 'array' });
+
+    /* grab first sheet */
+    const wsname: string = wb.SheetNames[0];
+    const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+    /* save data */
+    this.data = (XLSX.utils.sheet_to_json(ws, { header: 1 }) as AOA);
+  };
+
+  write(): XLSX.WorkBook {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'SheetJS');
+
+    return wb;
+  };
+
+  /* File Input element for browser */
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = (evt.target as DataTransfer);
+    if (target.files.length !== 1) { throw new Error('Cannot use multiple files'); }
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const ab: ArrayBuffer = e.target.result;
+      this.read(ab);
+    };
+    reader.readAsArrayBuffer(target.files[0]);
+    console.log (`got file name: ${target.files[0].name}`)
+  };
+
+  /* Import button for mobile */
+  async import() {
+    alert(`IMPORT NOT IMPLEMENTED YET (how to resolve FILE?)`)
+    try {
+      // const target: string = this.file.documentsDirectory || this.file.externalDataDirectory || this.file.dataDirectory || '';
+      // const dentry = await this.file.resolveDirectoryUrl(target);
+      // const url: string = dentry.nativeURL || '';
+      // alert(`Attempting to read SheetJSIonic.xlsx from ${url}`);
+      // const ab: ArrayBuffer = await this.file.readAsArrayBuffer(url, 'SheetJSIonic.xlsx');
+      // this.read(ab);
+    } catch (e: any) {
+      const m: string = e.message;
+      alert(m.match(/It was determined/) ? 'Use File Input control' : `Error: ${m}`);
+    }
+  };
+
+  /* Export button */
+  async export() {
+    const wb: XLSX.WorkBook = this.write();
+    const filename = 'SheetJSIonic.xlsx';
+    alert (`EXPORT NOT IMPLEMENTED YET (how to resolve FILE?)`)
+    try {
+      /* generate Blob */
+      const wbout: ArrayBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+
+      /* find appropriate path for mobile */
+     // const target: string = this.file.documentsDirectory || this.file.externalDataDirectory || this.file.dataDirectory || '';
+      //const dentry = await this.file.resolveDirectoryUrl(target);
+      //const url: string = dentry.nativeURL || '';
+
+      /* attempt to save blob to file */
+      //await this.file.writeFile(url, filename, wbout, { replace: true });
+      //alert(`Wrote to SheetJSIonic.xlsx in ${url}`);
+    } catch (e: any) {
+      if (e.message.match(/It was determined/)) {
+        /* in the browser, use writeFile */
+        XLSX.writeFile(wb, filename);
+      } else {
+        alert(`Error name: ${e.name}; msg: ${e.message}`);
+      }
+    }
+  };
+
+
+
+
+
+
+
+
 }
 
   // works - but Unused....
