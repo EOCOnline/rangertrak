e80118f20360c72a471b02c5729baa78ea93ef9e
JCornelison <gitKraken@VashonDesign.com>
2022-09-22 06:51:29 -0700

    Moving toward leaflet 1.9.0...

--- src/app/entry/mini-lmap.component.ts
+++ src/app/entry/mini-lmap.component.ts
@@ -42,7 +42,7 @@ L.Marker.prototype.options.icon = iconDefault;
   selector: 'mini-lmap',
   templateUrl: './mini-lmap.component.html',
   styleUrls: ['./mini-lmap.component.scss',
-    '../../../node_modules/leaflet/dist/leaflet.css'], // only seems to work when embedded in angula.json & Here! (chgs there REQUIRE restart!)]
+    '../../../node_modules/leaflet/dist/leaflet.css'], // only seems to work when embedded in angular.json & Here! (chgs there REQUIRE restart!)]
   providers: [SettingsService]
 })
 export class MiniLMapComponent extends AbstractMap implements OnInit, AfterViewInit, OnDestroy {
