868526eaf23b31c2567121e146c8ed297366a380
JCornelison <gitKraken@VashonDesign.com>
2022-10-09 14:47:25 -0700

    Clean up installable functionality

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core'
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
-import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
+import { faL, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
 import { MDCTopAppBar } from '@material/top-app-bar'
+// import { MatButton } from '@angular/material/button'
+// import { MatButtonModule } from '@angular/material/button'
 import { subscribeOn } from 'rxjs';
+import { InstallableService, LogService, SettingsService, SettingsType } from '../services';
 //https://material.io/components/app-bars-top/web#regular-top-app-bar
 
 @Component({
@@ -12,10 +15,21 @@ import { subscribeOn } from 'rxjs';
 })
 export class NavbarComponent implements OnInit {
 
+  private settings!: SettingsType
+  private id = 'Navbar Component'
+
+  isInstallable = false
   isNavigating = false
   faMapMarkedAlt = faMapMarkedAlt
 
-  constructor(private router: Router) {
+  constructor(
+    private log: LogService,
+    //private settingsService: SettingsService,
+    private installableService: InstallableService,
+    private router: Router
+  ) {
+    console.log("Navbar Component: constructor")
+
     this.router.events.subscribe(
       (event) => {
         if (event instanceof NavigationStart) {
@@ -26,16 +40,36 @@ export class NavbarComponent implements OnInit {
         }
       }
     )
+
+    if (installableService.installableEvent) {
+      this.isInstallable = true
+    }
+
+    /* REVIEW: unused?!
     const topAppBarElement = document.querySelector('.mdc-top-app-bar')
     if (!topAppBarElement) {
+      console.warn("Navbar Component: no topAppBarElement")
       return
     }
     const topAppBar = new MDCTopAppBar(topAppBarElement)
+    */
   }
 
   ngOnInit(): void {
+    this.log.info("Navbar Component: ngOnInit", this.id)
   }
+
   Search() {
-    console.log("Search is unimplemented yet")
+    this.log.error("Navbar Component: Search: unimplemented yet", this.id)
+  }
+
+  onInstallBtn() {
+    this.log.info("Navbar Component: User wants to install app!"), this.id
+
+    // From Angular Cookbook, pg 592
+    // https://web.dev/customize-install
+    // https://github.com/WICG/manifest-incubations
+
+    //! TODO: Implement me!!!
   }
 }
--- src/app/shared/services/index.ts
+++ src/app/shared/services/index.ts
@@ -5,12 +5,14 @@ export { PopupService } from "./popup.service"
 export { SettingsService, SecretType } from "./settings.service"
 export { ClockService } from "./clock.service"
 export { UpdateService } from "./update.service"
+export { InstallableService } from "./installable.service"
 
 export { FieldReportType, FieldReportsType, FieldReportStatusType, FieldReportSource } from "./field-report.interface"
 export { LocationType, undefinedAddressFlag, undefinedLocation } from "./location.interface"
 export { LogLevel, LogType, LogHeadings } from "./log.interface"
 export { RangerType, UnknownRanger } from "./ranger.interface"
 export { SettingsType } from "./settings.interface"
+
 
 //export { ShapeService } from "../unused/shape.service"
 //export { DataService } from "../unused/data.service"
