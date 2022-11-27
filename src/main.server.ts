2133ddef1263f0e6b0e566c303f6fa01f79e80c2
JCornelison <gitKraken@VashonDesign.com>
2022-11-27 12:04:59 -0800

    tried adding window from MockBrowser: fails

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,2 +1,7 @@
 
 export { AppServerModule } from './app/app.server.module';
+
+// https://www.thecodehubs.com/how-to-solve-window-is-not-defined-in-angular-11/
+const MockBrowser = require('mock-browser').mocks.MockBrowser;
+const mock = new MockBrowser();
+global['window'] = mock.getWindow();
